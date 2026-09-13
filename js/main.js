de47aac7bd40488c66196b2df11034a492b63f1e
sorlyx <evonne93327@gmail.com>
2026-09-13 14:22:16 +0800

    Add utility functions to main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function formatTime(d) { /* 原本的 formatTime 實作 */ }
+function escapeHtml(str) { /* 原本的 escapeHtml 實作 */ }
+function switchView(view, pushHistory = true) { /* 原本的 switchView 實作 */ }
+function toggleSidebarMenu() { /* 原本的 toggleSidebarMenu 實作 */ }
+function openSidebarMenu() { /* 原本的 openSidebarMenu 實作 */ }
+function closeSidebarMobile() { /* 原本的 closeSidebarMobile 實作 */ }
+function scheduleAutoGrowAfterLayoutShift() { /* 原本的 scheduleAutoGrowAfterLayoutShift 實作 */ }
+function handleBreadcrumbDblClick(e) { /* 原本的 handleBreadcrumbDblClick 實作 */ }
+function updateWorldBadge() { /* 原本的 updateWorldBadge 實作 */ }
+
+function setupHistoryNavigation() { /* 原本的 setupHistoryNavigation 實作 */ }
+function setupDeleteKeyShortcut() { /* 原本的 setupDeleteKeyShortcut 實作 */ }
+function setupGlobalKeyboardShortcuts() { /* 原本的 setupGlobalKeyboardShortcuts 實作 */ }
+function setupGlobalClickDismiss() { /* 原本的 setupGlobalClickDismiss 實作 */ }
